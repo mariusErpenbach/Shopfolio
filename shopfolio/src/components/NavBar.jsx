6e13d2c4eaddab03120c16994328365f103dc11c
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2022-01-19 23:09:23 +0100

    shopfolio setup

---
+++ shopfolio/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+const NavBar = () =>{
+
+
+    return (
+        <div id="navBarTop">
+        <main>
+        <input id="searchBar"></input>
+        </main>
+        <aside>
+            <div>Info</div>
+            <div>My Account</div>
+            <div>Wishlist</div>
+            <div>Shopping Cart</div>
+        </aside>
+        </div>
+    )
+}
+export default NavBar
