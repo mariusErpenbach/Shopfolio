b987e7b143e7751a5718ee1e1d68c631e5c04e51
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2023-03-02 23:52:09 +0100

    backend product database

--- shopfolio/frontend/src/components/databaseProducts.jsx
+++ shopfolio/frontend/src/components/databaseProducts.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 
 const databaseProducts = (props) =>{
-    const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'headstrong312',
-  database: 'mydb'
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL server');
-});
-
 return(<div>
 shit
 </div>)
