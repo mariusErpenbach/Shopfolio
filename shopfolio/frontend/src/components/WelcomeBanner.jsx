db68b44bbb26c9fe6f88f346a260d4747c77c0d7
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2023-04-26 00:02:20 +0200

    welcomeBanner

--- shopfolio/frontend/src/components/WelcomeBanner.jsx
+++ shopfolio/frontend/src/components/WelcomeBanner.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const welcomeBanner = () => {
 return (
 <div id="welcomeBanner">
+<button id="welcomeBanner_nextButton"><i class="fa-solid fa-angles-left"></i></button>
+<div id="welcomeText">
+    Summer Sale 66% Off!
+</div>
+
+<button id="welcomeBanner_prevButton"><i class="fa-solid fa-angles-right"></i></button>
 
 </div>)
 }
