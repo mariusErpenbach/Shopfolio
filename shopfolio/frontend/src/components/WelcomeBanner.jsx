ead7adf8611486ee7c2c5c55f8357fbcbc7ca55b
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2023-05-02 08:37:02 +0200

    banner

--- shopfolio/frontend/src/components/WelcomeBanner.jsx
+++ shopfolio/frontend/src/components/WelcomeBanner.jsx
@@ -5,7 +5,7 @@ return (
 <div id="welcomeBanner">
 <button id="welcomeBanner_nextButton"><i className="fa-solid fa-angles-left"></i></button>
 <div id="BannerContent"> 
-<img src="../img/banner.jpg"> </img>
+<img src="../img/banner.jpg" alt="Banner" />
 </div>
 <button id="welcomeBanner_prevButton"><i className="fa-solid fa-angles-right"></i></button>
 </div>)
