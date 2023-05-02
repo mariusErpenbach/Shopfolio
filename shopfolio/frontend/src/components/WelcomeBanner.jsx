774294dc5a9aeae15626d14dffcfd9312a1d060d
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2023-05-02 13:25:26 +0200

    welcomeBannerFunc

--- shopfolio/frontend/src/components/WelcomeBanner.jsx
+++ shopfolio/frontend/src/components/WelcomeBanner.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import banners from "./BannerCataloge.json"
+const WelcomeBanner = () => {
+   
+  const [bannerImageSrc, setbannerImageSrc] = useState(banners[0].imageSrc);
 
-const welcomeBanner = () => {
-return (
-<div id="welcomeBanner">
-<button id="welcomeBanner_nextButton"><i className="fa-solid fa-angles-left"></i></button>
-<div id="BannerContent"> 
-<img src="../img/banner.jpg" alt="Banner" />
-</div>
-<button id="welcomeBanner_prevButton"><i className="fa-solid fa-angles-right"></i></button>
-</div>)
-}
-export default welcomeBanner 
+  const nextBanner = () => {
+    const currentIndex = banners.findIndex(banner => banner.imageSrc === bannerImageSrc);
+    if (currentIndex === banners.length - 1) {
+      setbannerImageSrc(banners[0].imageSrc);
+    } else {
+      setbannerImageSrc(banners[currentIndex + 1].imageSrc);
+    }
+  };
+  const prevBanner = () => {
+  const currentIndex = banners.findIndex(banner => banner.imageSrc === bannerImageSrc);
+  if (currentIndex === 0) {
+    setbannerImageSrc(banners[banners.length - 1].imageSrc);
+  } else {
+    setbannerImageSrc(banners[currentIndex - 1].imageSrc);
+  }
+};
+  return (
+    <div id="welcomeBanner">
+      <button id="welcomeBanner_prevButton">
+        <i className="fa-solid fa-angles-right" onClick={nextBanner}></i>
+      </button>
+   
+        <img id="BannerImage" src={bannerImageSrc} alt="Banner"/>
+        
+        <p> hello world</p>
+        
+        <div id="BannerCounterBox"> 
+            <div className="bannerCounter"></div>
+            <div className="bannerCounter"></div>
+            <div className="bannerCounter"></div> 
+        </div>
+      
+      <button id="welcomeBanner_nextButton" onClick={prevBanner}>
+        <i className="fa-solid fa-angles-left"></i>
+      </button>
+    </div>
+  );
+};
+
+export default WelcomeBanner;
